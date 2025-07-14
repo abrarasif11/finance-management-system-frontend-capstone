6a799f85662e71b045a49406e05fcafdfb7cd3e7
abrarasif11 <faasif929@gmail.com>
2025-07-15 00:59:28 +0600

    correction-on-service-section

--- src/components/constants/index.jsx
+++ src/components/constants/index.jsx
@@ -1,9 +1,6 @@
-import { BotMessageSquare } from "lucide-react";
-import { BatteryCharging } from "lucide-react";
-import { Fingerprint } from "lucide-react";
-import { ShieldHalf } from "lucide-react";
-import { PlugZap } from "lucide-react";
-import { GlobeLock } from "lucide-react";
+import { Calculator, CircleDollarSign, HandCoins, Landmark } from "lucide-react";
+import LoanIcon from '../../assets/money-bag.png';
+
 
 
 export const navItems = [
@@ -15,41 +12,36 @@ export const navItems = [
   ];
   export const features = [
     {
-      icon: <BotMessageSquare />,
+      icon: <CircleDollarSign />,
       text: "Expense and Income Tracking",
       description:
         "Log expenses and income by category to understand spending patterns and manage budgets.",
     },
     {
-      icon: <Fingerprint />,
+      icon: <Calculator />,
       text: "Budgeting",
       description:
         "Set monthly or annual budgets with alerts to help users stick to financial limits.",
     },
     {
-      icon: <ShieldHalf />,
-      text: "Savings and Goal Planning",
+      icon: <HandCoins />,
+      text: "Savings Planning",
       description:
         "Track savings goals (like vacations or emergency funds) with progress updates.",
     },
     {
-      icon: <BatteryCharging />,
+      icon: <Landmark />,
       text: "Investment Management",
       description:
         "Monitor personal investments, returns, and portfolio growth.",
     },
     {
-      icon: <PlugZap />,
-      text: "Debt and Loan Management",
+      icon: <img src={LoanIcon} />,
+      text: "Loan Management",
       description:
         "Track bills, debts, and repayment schedules to avoid late fees and manage liabilities.",
     },
-    {
-      icon: <GlobeLock />,
-      text: "Tax Preparation",
-      description:
-        "Store tax documents and track deductible expenses for simplified tax filing.",
-    },
+    
   ];
   export const checklistItems = [
     {
